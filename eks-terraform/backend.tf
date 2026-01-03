887705a5d188058d3ba7b33cace6b79f1b5421b7
ayaallg <aya.allag@univ-constantine2.dz>
2026-01-04 00:03:02 +0100

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ml-terraform-state-73c66080"  # <--- REMPLACE ICI par ton ID
+    bucket = "ml-terraform-state-8749f2b4"
     key    = "k8/terraform.tfstate"
     region = "us-east-1"
   }
