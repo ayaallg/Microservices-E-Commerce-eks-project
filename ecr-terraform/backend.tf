3aac407cf0dc99069611f3af12d2f93defe22dde
ayaallg <aya.allag@univ-constantine2.dz>
2026-01-04 01:26:17 +0100

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -5,13 +5,13 @@ terraform {
       version = ">= 5.25.0"
     }
   }
-
-  backend "s3" {
-  bucket = "melissa-lab-terraform-state"
-  key    = "ecr/terraform.tfstate"
-  region = "us-east-1"
-
-  }
+environment {
+    // Remplace par ton vrai bucket qui finit par 73c66080
+    TF_BACKEND_BUCKET = "ml-terraform-state-73c66080" 
+    TF_REGION         = "us-east-1"
+    TERRAFORM_DIR     = "ecr-terraform"
+}
 
   required_version = ">= 1.6.3"
 }
+
