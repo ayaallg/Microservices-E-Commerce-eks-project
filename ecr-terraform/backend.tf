a703c3fcd6fae8601ba6956787a3bd30e49b28fb
ayaallg <aya.allag@univ-constantine2.dz>
2026-01-04 01:31:50 +0100

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -5,13 +5,10 @@ terraform {
       version = ">= 5.25.0"
     }
   }
-environment {
-    // Remplace par ton vrai bucket qui finit par 73c66080
-    TF_BACKEND_BUCKET = "ml-terraform-state-73c66080" 
-    TF_REGION         = "us-east-1"
-    TERRAFORM_DIR     = "ecr-terraform"
-}
+
+  # Ce bloc doit impérativement rester vide {}. 
+  # Jenkins injectera les valeurs (bucket, key, region) lors du "terraform init".
+  backend "s3" {}
 
   required_version = ">= 1.6.3"
 }
-
